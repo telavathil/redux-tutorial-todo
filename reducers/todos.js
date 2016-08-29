cdd15da9253eeccda3129460e43e5213d5e6a60b
Tobin Elavathil <tobin.elavathil@gmail.com>
2016-08-28 20:30:34 -0400

    NewTodo function implimented with unshift

--- reducers/todos.js
+++ reducers/todos.js
@@ -3,7 +3,7 @@ import Immutable from 'immutable'
 export default (state = Immutable.List(['Code More!']), action) => {
   switch(action.type){
     case 'addTodo':
-      return state.push(action.todo)
+      return state.unshift(action.todo)
     default:
       return state
   }
